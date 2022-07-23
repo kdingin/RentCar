2bebae34dc3fc81f9a9840cddf145ff826fdab32
fatih Küçükdingin <fatihkdingin@gmail.com>
2022-07-23 03:52:47 +0300

    23.07.2022

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManagerTest3();
-            //CarManagerTest2();
+           // CarManagerTest2();
 
             // ColorTest2();
 
